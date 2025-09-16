9a584191b3ce3955ce6a5e32224ba953b15023e3
austinalex1995 <aealex1995@gmail.com>
2025-09-16 16:23:03 -0400

    feat: added FAQs page

---
+++ app/pages/faq.tsx
@@ -0,0 +1,17 @@
+import BasePage from "components/BasePage";
+import { StyleSheet } from "react-native";
+
+
+const FAQs = () => {
+
+    return (
+        <BasePage>
+        </BasePage>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default FAQs;
