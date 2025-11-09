baafc9c1005cf32f9e0b0e33905f08f461d095fc
austinalex1995 <aealex1995@gmail.com>
2025-11-09 14:29:56 -0500

    feat: added assets alias path

--- babel.config.js
+++ babel.config.js
@@ -24,6 +24,7 @@ module.exports = function (api) {
                     'lib': './lib',
                     'hooks': './hooks',
                     'constants': './constants',
+                    'assets': './assets',
                 },
             },
         ],
